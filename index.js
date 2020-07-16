af58d81372d696fe1e5d278b8ffa015353a61815
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2020-07-16 18:38:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,31 @@ client.once('ready', () => {
     console.log('Soup is Online!');
 })
 
+client.on('message', message =>{
+    if(!message.content.startswith(prefix) || message.author.client) return;
 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
 
+    if (command == 'youtube'){
+        message.channel.send("https://www.youtube.com/channel/UC0KW9Y85cFkrZyPkWrNVRUQ");
+    } else if (command == 'twitch'){
+        message.channel.send("https://twitch.tv/dorito__soup", "https://twitch.tv/daecu8603");
+    } 
+})
 
+client.on('message', message =>{
+    if(message.content === "fuck", "FUCK"){
+        message.channel.reply('SWORE DETECTOR HAS DECTECTED THE USE OF THE F-WORD');
+    } else if (message.content == "fruck" || "FRUCK"){
+        message.channel.reply('SWORE DETECTOR HAS DECTECTED THE USE OF THE FR-WORD');
+    } else if (message.content == "hello?" || "HELLO?" || "Hello?" || "hello" || "HELLO" || "Hello"){
+        message.channel.send('Hello :)');
+    } else if (message.content == "ping"){
+        message.channel.reply('Pong!')
+    }
 
-
-
+})
 
 
 
