f7931b1eb4ad52211048ce0843a0e3e09ddce008
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2020-07-16 18:50:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ client.on('message', message =>{
 
 client.on('message', message =>{
     if(message.content === "fuck", "FUCK"){
-        message.channel.reply('SWORE DETECTOR HAS DECTECTED THE USE OF THE F-WORD');
+        message.reply('SWORE DETECTOR HAS DECTECTED THE USE OF THE F-WORD');
     } else if (message.content === "fruck", "FRUCK"){
-        message.channel.reply('SWORE DETECTOR HAS DECTECTED THE USE OF THE FR-WORD');
+        message.reply('SWORE DETECTOR HAS DECTECTED THE USE OF THE FR-WORD');
     } else if (message.content === "ping"){
-        message.channel.reply('Pong!')
+        message.reply('Pong!')
     }
 
 })
