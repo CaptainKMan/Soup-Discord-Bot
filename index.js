3ec712f5d40bd0999a39c171996c8d1e04f9c73e
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2020-07-17 15:05:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -40,25 +40,19 @@ client.on('message', message => {
             return message.channel.send(`You didn't specify a Soup member, ${message.author}!`);
         }
         else if (args[0] === 'fluxsoup'){
-            const exampleEmbed = new Discord.MessageEmbed()
+            const fluxsoup = new Discord.MessageEmbed()
             .setColor('#0099ff')
             .setTitle('Some title')
             .setURL('https://discord.js.org/')
             .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
             .setDescription('Some description here')
             .setThumbnail('https://i.imgur.com/wSTFkRM.png')
-            .addFields(
-                { name: 'Regular field title', value: 'Some value here' },
-                { name: '\u200B', value: '\u200B' },
-                { name: 'Inline field title', value: 'Some value here', inline: true },
-                { name: 'Inline field title', value: 'Some value here', inline: true },
-            )
             .addField('Inline field title', 'Some value here', true)
             .setImage('https://i.imgur.com/wSTFkRM.png')
             .setTimestamp()
             .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
         
-        message.channel.send(exampleEmbed);
+        message.channel.send(fluxsoup);
         }
         else if (args[0] === 'doritosoup'){
             return message.channel.send('*Under Construction*')
