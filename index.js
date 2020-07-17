abd92da3809cdab2a7139254d71aba11dc8b3807
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2020-07-17 18:14:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -45,8 +45,8 @@ client.on('message', message => {
                 .setTitle('FluxSoup. (a.k.a Scott)')
                 .setURL('')
                 .setDescription('The founder and supreme leader of the Soup Kitchen.')
-                .setThumbnail('https://cdn.discordapp.com/avatars/142126035301957632/08b1a2878dfc107da0fa93b9231bf440.png')
-                .setFooter('Last Updated by DoritoSoup on 7/17/2020.', 'https://cdn.discordapp.com/avatars/357663989418688513/efba3105379791574e341696fd4a5334.png');
+                .setThumbnail('https://cdn.discordapp.com/avatars/142126035301957632/08b1a2878dfc107da0fa93b9231bf440.png?size=1024')
+                .setFooter('Last Updated by DoritoSoup on 7/17/2020.', 'https://cdn.discordapp.com/avatars/357663989418688513/efba3105379791574e341696fd4a5334.png?size=1024');
 
             return message.channel.send("fluxsoup");
         }
