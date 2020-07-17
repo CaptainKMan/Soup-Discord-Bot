26aa623306ff46b360f78e2c04af627d7cc58cbe
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2020-07-17 12:27:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ client.on('message', message =>{
     } else if (message.content === 'Lmao'){
         message.channel.send('https://cdn.dlcordapp.com/attachments/486699197131915266/710452259770728448/cute_koishi.png');
     } else if (message.content === 'help'){
-        message.author.send('https://github.com/CaptainKMan/Readme/blob/953ba0201c9f4140467dfc4228e719cbd583a0c2/README.md');
+        author.send('https://github.com/CaptainKMan/Readme/blob/953ba0201c9f4140467dfc4228e719cbd583a0c2/README.md');
     }
 
 });
