ccff760a58d52d86ae7071bc6054a6a213c0b21e
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2020-07-17 14:16:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -41,6 +41,9 @@ client.on('message', message =>{
         }
         else if (args[0] === 'MemeSoup.') {
             return message.channel.send('*Under Construction*');
+        if (args[0] === 'FluxSoup.'){
+            return message.channel.send('*Under Construction*');
+        }
         }
         message.channel.send(`${args[0]} is not a Soup!`);
     }
