3be8dfaa1948d11e348d09b6dfb03622d47c3086
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2020-07-17 12:56:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -59,11 +59,11 @@ client.on('message', message =>{
         message.channel.send('Ping!')
     } else if (message.content === 'Pong'){
         message.channel.send('Ping!')
-    } else if (message.content === 'lmao'){
+    } else if (message.content === 'lol'){
         message.channel.send('https://cdn.dlcordapp.com/attachments/486699197131915266/710452259770728448/cute_koishi.png');
     } else if (message.content === 'LMAO'){
         message.channel.send('https://cdn.dlcordapp.com/attachments/486699197131915266/710452259770728448/cute_koishi.png');
-    } else if (message.content === 'Lmao'){
+    } else if (message.content === 'LoL'){
         message.channel.send('https://cdn.dlcordapp.com/attachments/486699197131915266/710452259770728448/cute_koishi.png');
     }
 
