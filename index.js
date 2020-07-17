ef238bba5f2c3a52c5312cd5e4c5cbdc25314851
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2020-07-17 13:37:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,26 +36,8 @@ client.on('message', message =>{
     } else if (command === 'rick2'){
         message.channel.send('https://youtu.be/dQw4w9WgXcQ')
     } else if (command === 'member'){
-        if (command === 'MemeSoup.'){
-            const exampleEmbed = new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('Some title')
-	.setURL('https://discord.js.org/')
-	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-	.setDescription('Some description here')
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/wSTFkRM.png')
-	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-
-    channel.send(exampleEmbed);
+        if (args === 'MemeSoup.'){
+            message.channel.send('helo');
         }
     }
 
