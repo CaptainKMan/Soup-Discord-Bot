c48368b9997b2a1b16df657b3e8be46dd5b896bf
CaptainKMan <68390690+CaptainKMan@users.noreply.github.com>
2020-07-17 14:27:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,81 +6,81 @@ const prefix = '|';
 
 client.once('ready', () => {
     console.log('Soup is Online!');
-    client.user.setActivity("with MemeSoup's Internet", { type: 'PLAYING'}).catch(console.error);
+    client.user.setActivity("with MemeSoup's Internet", { type: 'PLAYING' }).catch(console.error);
 });
 
-client.on('message', message =>{
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if (command === 'youtube'){
+    if (command === 'youtube') {
         message.channel.send('```https://www.youtube.com/channel/UC0KW9Y85cFkrZyPkWrNVRUQ```');
-    } else if (command === 'twitch'){
-        message.channel.send('```https://twitch.tv/dorito__soup```'), 
-        message.channel.send('```https://twitch.tv/daecu8603```');
-    } else if (command === 'members'){
-        message.channel.send('The current Soup Kitchen members are:'), 
-        message.channel.send('<@142126035301957632>'), 
-        message.channel.send('<@357663989418688513>'), 
-        message.channel.send('<@399172695213735936>'), 
-        message.channel.send('<@622181058841935883>'), 
-        message.channel.send('<@330761771943985155>'), 
-        message.channel.send('<@513683756012273664>'), 
-        message.channel.send('<@398587405369737217>');
-    } else if (command === 'help'){
+    } else if (command === 'twitch') {
+        message.channel.send('```https://twitch.tv/dorito__soup```'),
+            message.channel.send('```https://twitch.tv/daecu8603```');
+    } else if (command === 'members') {
+        message.channel.send('The current Soup Kitchen members are:'),
+            message.channel.send('<@142126035301957632>'),
+            message.channel.send('<@357663989418688513>'),
+            message.channel.send('<@399172695213735936>'),
+            message.channel.send('<@622181058841935883>'),
+            message.channel.send('<@330761771943985155>'),
+            message.channel.send('<@513683756012273664>'),
+            message.channel.send('<@398587405369737217>');
+    } else if (command === 'help') {
         message.author.send('https://raw.githubusercontent.com/CaptainKMan/Readme/master/README.md');
-    } else if (command === 'rick'){
+    } else if (command === 'rick') {
         message.channel.send('https://images.app.goo.gl/oTt3wVVgrqo8Wv7Y7');
-    } else if (command === 'rick2'){
+    } else if (command === 'rick2') {
         message.channel.send('https://images.app.goo.gl/dhwsW3tcXbeu1vud7')
-    } else if (command === 'member'){
+    } else if (command === 'member') {
         if (!args.length) {
             return message.channel.send(`You didn't specify a Soup member, ${message.author}!`);
         }
-        else if (args[0] === 'MemeSoup.') {
+        else if (args[0] === 'MemeSoup.'){
             return message.channel.send('*Under Construction*');
-        if (args[0] === 'FluxSoup.'){
+        if (args[1] === 'FluxSoup.'){
             return message.channel.send('*Under Construction*');
         }
         }
-        message.channel.send(`${args[0]} is not a Soup!`);
     }
     
     
-
+    
+    message.channel.send(`${args[0]} is not a Soup!`);
 });
 
 
-client.on('message', message =>{
-    if(message.content === "fuck"){
+client.on('message', message => {
+    if (message.content === "fuck") {
         message.reply('SWORE DETECTOR HAS DECTECTED THE USE OF THE F-WORD');
-    } else if (message.content === "fruck"){
+    } else if (message.content === "fruck") {
         message.reply('SWORE DETECTOR HAS DECTECTED THE USE OF THE FR-WORD');
-    } else if (message.content === "Fruck"){
+    } else if (message.content === "Fruck") {
         message.reply('SWORE DETECTOR HAS DECTECTED THE USE OF THE FR-WORD');
-    } else if (message.content === "FRUCK"){
+    } else if (message.content === "FRUCK") {
         message.reply('SWORE DETECTOR HAS DECTECTED THE USE OF THE FR-WORD');
-    } else if (message.content === "FUCK"){
+    } else if (message.content === "FUCK") {
         message.reply('SWORE DETECTOR HAS DECTECTED THE USE OF THE F-WORD');
-    } else if (message.content === "Fuck"){
+    } else if (message.content === "Fuck") {
         message.reply('SWORE DETECTOR HAS DECTECTED THE USE OF THE F-WORD');
-    } else if (message.content === "fuck"){
+    } else if (message.content === "fuck") {
         message.reply('SWORE DETECTOR HAS DECTECTED THE USE OF THE F-WORD');
-    } else if (message.content === 'ping'){
+    } else if (message.content === 'ping') {
         message.channel.send('Pong!')
-    } else if (message.content === 'Ping'){
+    } else if (message.content === 'Ping') {
         message.channel.send('Pong!')
-    } else if (message.content === 'pong'){
+    } else if (message.content === 'pong') {
         message.channel.send('Ping!')
-    } else if (message.content === 'Pong'){
+    } else if (message.content === 'Pong') {
         message.channel.send('Ping!')
-    } else if (message.content === 'lol'){
+    } else if (message.content === 'lol') {
         message.channel.send('https://cdn.dlcordapp.com/attachments/486699197131915266/710452259770728448/cute_koishi.png');
-    } else if (message.content === 'LMAO'){
+    } else if (message.content === 'LMAO') {
         message.channel.send('https://cdn.dlcordapp.com/attachments/486699197131915266/710452259770728448/cute_koishi.png');
-    } else if (message.content === 'LoL'){
+    } else if (message.content === 'LoL') {
         message.channel.send('https://cdn.dlcordapp.com/attachments/486699197131915266/710452259770728448/cute_koishi.png');
     }
 
